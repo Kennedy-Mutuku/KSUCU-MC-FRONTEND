2e32c0b2c028946f193a98610ddf6430ee0fac74
ken <ken@localhost.localdomain>
2025-09-01 10:40:36 +0000

    about us fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          fontawesome: ['@fortawesome/fontawesome-svg-core', '@fortawesome/free-solid-svg-icons', '@fortawesome/free-regular-svg-icons', '@fortawesome/free-brands-svg-icons', '@fortawesome/react-fontawesome'],
+          pdf: ['jspdf', 'jspdf-autotable'],
+          ui: ['bootstrap', 'react-icons', 'lucide-react']
+        }
+      }
+    }
+  }
 })
